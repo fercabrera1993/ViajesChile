b3ee021b18b015db595daef63e7551ab6d538a24
fercabrera1993 <103543511+fercabrera1993@users.noreply.github.com>
2022-04-28 21:16:23 -0400

    Add files via upload

--- ViajesChile/assets/js/index.js
+++ ViajesChile/assets/js/index.js
@@ -6,3 +6,35 @@ $(document).ready(function(){
 
 });
 
+/* SMOOTHSCROLL*/
+$("a").on('click', function(event) {
+
+// Make sure this.hash has a value before overriding default behavior
+if (this.hash !== "") {
+// Prevent default anchor click behavior
+event.preventDefault();
+
+// Store hash
+var hash = this.hash;
+
+// Using jQuery's animate() method to add smooth page scroll
+// The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+$('html, body').animate({
+scrollTop: $(hash).offset().top
+}, 800, function(){
+
+// Add hash (#) to URL when done scrolling (default click behavior)
+window.location.hash = hash;
+});
+} // End if
+});
+
+
+/*ALERTA CORREO: CLICK Y ALERT*/
+$(document).ready(function(){
+
+$('#enviarComentario').click(function(){
+        alert("Gracias por tu mensaje.");
+        console.log('Comentario enviado');
+});
+});
